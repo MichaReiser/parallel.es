caf0b3ca8b3775425fe336c35f3d4fccf45e06a7
Micha Reiser <micha@famreiser.ch>
2016-09-01 17:15:32 +0200

    Change to awesome-typescript-loader and simplify build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
         browser: "./src/browser/index.ts"
     },
     debug: true,
-    devtool: "inline-source-map",
+    devtool: "#inline-source-map",
     output: {
         library: "parallel-es",
         libraryTarget: "umd",
@@ -25,19 +25,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.ts$/,
-                include: [path.resolve(__dirname, "./src/browser/slave")],
-                loader: 'ts-loader?instance=webworker&configFileName=./src/browser/slave/tsconfig.json'
-            },
-            {
-                test: /\.ts$/,
-                include: [path.resolve(__dirname, "./src/common"), path.resolve(__dirname, "./src/browser")],
-                exclude: [path.resolve(__dirname, "./src/browser/slave")],
-                loader: 'ts-loader?instance=browser&configFileName=./src/browser/tsconfig.json'
-            },
-            {
-                test: /\.ts$/,
-                include: [path.resolve(__dirname, "./test")],
-                loader: 'ts-loader?instance=tests&configFileName=./test/tsconfig.json'
+                loader: 'awesome-typescript-loader'
             }
         ]
     },
