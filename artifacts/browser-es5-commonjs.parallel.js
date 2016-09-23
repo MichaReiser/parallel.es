90ead6fb9b8751d220e704daa91dde53116bb16a
Travis CI <micha@famreiser.ch>
2016-09-23 08:30:06 +0000

    Deploy to GitHub Pages: 251bf215268a1b3010aabfd3fef4677884bf1664

---
+++ artifacts/browser-es5-commonjs.parallel.js
@@ -0,0 +1,2 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports["parallel-es"]=o():e["parallel-es"]=o()}(this,function(){return webpackJsonpparallel_es([3,2],{59:function(e,o,t){var p=t(6)["default"];e.exports=p}},[59])});
+//# sourceMappingURL=browser-es5-commonjs.parallel.js.map
