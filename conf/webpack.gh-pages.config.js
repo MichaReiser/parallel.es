d827f0d6159b267d3f8b96deeb01eee0b1804b7d
Micha Reiser <micha@famreiser.ch>
2016-10-21 23:39:15 +0200

    Fix gh-pages build

--- conf/webpack.gh-pages.config.js
+++ conf/webpack.gh-pages.config.js
@@ -6,12 +6,15 @@ var CommonsChunkPlugin = require("webpack").optimize.CommonsChunkPlugin;
 
 module.exports = new Config().extend("conf/webpack.prod.config.js").merge({
     entry: {
-        "browser-es5-commonjs": "./src/browser/index-commonjs",
+        "browser-es5-commonjs": "./src/api/browser-commonjs",
         "example": "./example/browser-example.ts",
         "performance-measurement": "./example/performance-measurement.ts"
     },
     output: {
         path: path.resolve(__dirname, "../gh-pages-out/artifacts")
+    },
+    module: {
+        noParse: [ /benchmark\/benchmark\.js/ ]
     },
     devtool: "#source-map",
     plugins: [
