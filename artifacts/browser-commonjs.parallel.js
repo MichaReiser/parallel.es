f8928048129bbb6af412a868d800f81d668973c2
Travis CI <micha@famreiser.ch>
2016-10-21 22:02:28 +0000

    Deploy to GitHub Pages: 9568230b82c8be1c24744c253e041fb047297ed6

--- artifacts/browser-commonjs.parallel.js
+++ artifacts/browser-commonjs.parallel.js
@@ -1,2 +1,2 @@
-!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports["parallel-es"]=o():e["parallel-es"]=o()}(this,function(){return webpackJsonpparallel_es([2,3],{59:function(e,o,t){var p=t(6)["default"];e.exports=p}},[59])});
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports["parallel-es"]=o():e["parallel-es"]=o()}(this,function(){return webpackJsonpparallel_es([2,3],{58:function(e,o,t){var p=t(6)["default"];e.exports=p}},[58])});
 //# sourceMappingURL=browser-commonjs.parallel.js.map
