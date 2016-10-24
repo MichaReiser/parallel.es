17cd2e1ff3ec6c1ae8c59eb4f3f2c62daec74965
Micha Reiser <micha@famreiser.ch>
2016-10-24 10:45:07 +0200

    Extract examples and performance meassurement

--- conf/webpack.dev.config.js
+++ conf/webpack.dev.config.js
@@ -1,33 +1,14 @@
 var Config = require("webpack-config").Config;
-var CommonsChunkPlugin = require("webpack").optimize.CommonsChunkPlugin;
 
-module.exports = new Config().extend({
-    "conf/webpack.base.config.js": function (config) {
-        delete config.entry;
-        delete config.output.library;
-        delete config.output.libraryTarget;
-
-        return config;
-    }
-}).merge({
-    entry: {
-        example: "./example/browser-example.ts",
-        "performance-measurement": "./example/performance-measurement.ts"
-    },
+module.exports = new Config().extend("conf/webpack.base.config.js").merge({
     devtool: "#inline-source-map",
     output: {
         pathinfo: true
-    },
-    module: {
-        noParse: [ /benchmark\/benchmark\.js/ ]
     },
     watch: true,
     devServer: {
         stats: {
             chunks: false
         }
-    },
-    plugins: [
-        new CommonsChunkPlugin("browser")
-    ]
+    }
 });
