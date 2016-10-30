ec7b0da6b2a46e394221f2c7378230275c87cbf3
Micha Reiser <micha@famreiser.ch>
2016-10-30 09:22:50 +0100

    Update dependencies

--- conf/webpack.dev.config.js
+++ conf/webpack.dev.config.js
@@ -2,7 +2,16 @@ var Config = require("webpack-config").Config;
 
 const FILE_NAME = "[name].parallel-es6.js";
 
-module.exports = new Config().extend("conf/webpack.base.config.js").merge({
+module.exports = new Config().extend({
+    "conf/webpack.base.config.js": function (config) {
+        const awesomeLoader = config.module.loaders.find(function (loader) {
+            return loader.loader === "awesome-typescript-loader";
+        });
+
+        awesomeLoader.query.useBabel = false;
+        return config;
+    }
+}).merge({
     devtool: "#inline-source-map",
     output: {
         filename: FILE_NAME,
